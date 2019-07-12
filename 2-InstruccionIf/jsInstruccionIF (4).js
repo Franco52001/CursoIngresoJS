4c088bfffecf44d90cb64b1af375c2d292560a4b
Franco52001 <52410416+Franco52001@users.noreply.github.com>
2019-07-12 12:17:51 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,16 @@
 function mostrar()
 {
 //tomo la edad  
+    var edad
 
+    edad=document.getElementById("edad").value;
 
-
+    if(edad>=13){
+     
+    if(edad<=17){ 
+    
+    alert("Adolescente");
+    
+    }
+    }
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,19 @@
 function mostrar()
 {
 //tomo la edad  
+    var edad
+
+    edad=document.getElementById("edad").value;
+    
+   if(edad<13){
+    
+    alert("No es un adolescente");
+   }
+    if(edad>17){
+        
+    alert("No es un adolescente");}  
+
+    
 
 
 }//FIN DE LA FUNCIÓN
