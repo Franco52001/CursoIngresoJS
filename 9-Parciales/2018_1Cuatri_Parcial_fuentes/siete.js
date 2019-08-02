46bda01a1d751662a0f9da211cbc67ae4da72261
Franco52001 <52410416+Franco52001@users.noreply.github.com>
2019-08-02 03:08:08 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,46 @@
 function mostrar()
 {
+    var nota;
+    var sexo;
+    var contador = 0;
+    var acumulador = 0;
+    var promedio;
+    var notamin;
+    var sexomin;
+    var mensaje;
+    var contadorvaron = 0;
+
+    while (contador < 5)
+    {
+        contador ++;
+        nota = prompt("Ingrese nota");
+        sexo = prompt("Ingrese su genero");
+        nota = parseInt(nota)
+
+        acumulador = acumulador + nota;
+
+        
+        if(contador == 1)
+        {
+            notamin = nota;
+            sexomin = sexo;
+        }
+
+        else if(nota < notamin)
+        {
+            notamin = nota;
+            sexomin = sexo;
+        }
+
+        if(nota > 5 && sexo == "m")
+        {
+            contadorvaron++;
+        }
+
+           
+    }
+
+    alert(("El promedio de notas es " + acumulador / 5) + (" Esta persona es " + sexomin + "  y saco un " + notamin ) + (" La cantidad de varones que sacaron una nota mayor a 6 es " + contadorvaron));
+
 
 }
