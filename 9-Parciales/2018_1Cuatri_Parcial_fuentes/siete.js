70170083b1970ffd0c582da5b6bb4507aadc025e
alumno <alumno>
2019-08-05 12:17:54 -0300

    git

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -2,10 +2,8 @@ function mostrar() {
     var nota;
     var sexo;
     var acumulador = 0;
-    var promedio;
     var notamin = 0;
     var sexomin;
-    var mensaje;
     var contadorvaron = 0;
 
 
@@ -19,17 +17,22 @@ function mostrar() {
         nota = parseInt(nota)
 
         sexo = prompt("Ingrese su genero");
-        while (sexo != "f" && sexo != "m") {
+        while (sexo != "f" && sexo != "m" && sexo != "M" && sexo != "F") {
             sexo = prompt("Genero no valido, ingrese nuevamente");
         }
         acumulador = acumulador + nota;   
                                             //Estas dos ****** no funcionaron porque los puse afuera del for
-        if (nota > 5 && sexo == "m") {  //este no funciona con || 
+        if (nota > 5 && sexo == "m" || sexo == "M") {  //este no funciona con || 
             contadorvaron++;
         }
-        if (contador == 1 || notamin < nota)  { //esta ***** no andaba porque soy tarado y no valide el notamin
-            notamin = nota;                        //no se porque funciono afuera del for pero aca esta mejor
+        if(contador == 0)
+        {
+            notamin = nota; 
             sexomin = sexo;
+        }
+        if ( notamin > nota)  { //esta ***** no andaba porque soy tarado y no valide el notamin
+            notamin = nota;                        //no se porque funciono afuera del for pero aca esta mejor
+            sexomin = sexo;                            //contador == 1 estaba antes y no funcionaba lpm
         }
     }
         alert(("El promedio de notas es " + acumulador / 5) + (" Esta persona es " + sexomin + "  y saco un " + notamin) + (" La cantidad de varones que sacaron una nota mayor a 6 es " + contadorvaron));
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
@@ -1,4 +1,45 @@
 function mostrar()
 {
+    var nota;
+    var sexo;
+    var acumulador = 0
+    var sexoMin;
+    var notaMin;
+    var contadorVaron = 0;
+    
+    for(contador = 0 ; contador < 5 ; contador++ )
+    {
+        nota = prompt("Ingrese nota");
+        while(isNaN(nota) || nota < 0 || nota > 10)
+        {
+            nota = prompt("No es una nota valida, ingrese nuevamente");
+        }
+        nota = parseInt(nota)
+
+        sexo = prompt("Ingrese su sexo");
+        while(sexo !="m" && sexo !="f" && sexo !="M" && sexo != "F")
+        {
+            sexo = prompt("No es un sexo valido, ingrese nuevamente")
+        }
+
+        acumulador = acumulador + nota
+        
+        if(contador == 0) // aca se debe poner contador == 0 si pongo == 1 no me lo toma
+        {
+            notaMin = nota;
+            sexoMin = sexo;
+
+        }
+        if(nota < notaMin)
+        {
+            notaMin = nota;
+            sexoMin = sexo;
+        }
+
+        if(nota > 5 && sexo == "m" || sexo == "M"){
+            contadorVaron++;
+        }
+    }
+    alert("El promedio total de las notas es " + acumulador/5 + " Esta persona saco " + notaMin + " y es " + sexoMin + " y la cantidad de varones con nota mayor a 6 es " + contadorVaron)
 
 }
